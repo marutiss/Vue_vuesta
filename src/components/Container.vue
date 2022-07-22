a4bfb60f1e43b5895b7f07e6b9fb8bb0dd7c2293
marutiss <235kiki@naver.com>
2022-07-22 16:58:37 +0900

    CSS작성완료

---
+++ src/components/Container.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Post />
+  </div>
+</template>
+
+<script>
+import Post from './Post.vue'; //경로 주의
+
+export default {
+    name: 'Container',
+    components:{
+        Post: Post,
+    }
+};
+</script>
+
+<style>
+</style>
